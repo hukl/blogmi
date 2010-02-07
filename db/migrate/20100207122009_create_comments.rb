8ae1e260464898c66ac54fbc8a123882a7897b38
hukl <contact@smyck.org>
2010-02-07 15:04:13 +0100

    fixed Comment migration

--- db/migrate/20100207122009_create_comments.rb
+++ db/migrate/20100207122009_create_comments.rb
@@ -1,15 +1,16 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.integer :post_id
-      t.string :author
-      t.string :author_email
-      t.author_url :
-      t.author_ip :
-      t.author_agent :
-      t.text :body
-      t.boolean :approved
-      t.integer :user_id
+      t.integer  :post_id
+      t.string   :author
+      t.string   :author_email
+      t.string   :author_url
+      t.string   :author_ip
+      t.string   :author_agent
+      t.text     :body
+      t.boolean  :approved
+      t.integer  :user_id
+      t.string   :comment_type
 
       t.timestamps
     end
